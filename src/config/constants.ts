54f3df487b59e0bd30bc78aa152fe5f1266b6635
pompom <kitokito532@gmail.com>
2024-08-10 17:14:35 +0900

    サムネイル画像を生成する

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,2 +1,7 @@
+import path from 'path';
+
 export const VIDEO_DIRECTORY = "E:/BT/DOWN";
 export const VIDEO_EXTENSIONS = [".mp4", ".mkv", ".mov", ".avi", ".wmv"];
+
+export const THUMBNAIL_DIRECTORY = path.join(__dirname, "../../public/thumbnails");
+export const THUMBNAIL_SIZE = "768x432";
---
+++ src/services/thumbnailService.ts
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import path from 'path';
+import ffmpeg from 'fluent-ffmpeg';
+import { THUMBNAIL_DIRECTORY, THUMBNAIL_SIZE } from "../config/constants";
+
+/**
+ * 動画のサムネイル画像を生成する
+ * @param videoPath 動画ファイルのパス
+ * @param destDir 生成先ディレクトリパス
+ */
+export function generateThumbnailFile(videoPath: string, destDir: string = THUMBNAIL_DIRECTORY) {
+    createThumbnailDirectory();
+    ffmpeg(videoPath).screenshots({
+        filename: `thumbnail-${path.parse(videoPath).name.replace(/ /g, "_")}.png`,
+        count: 1,
+        folder: destDir,
+        size: THUMBNAIL_SIZE,
+    });
+}
+
+function createThumbnailDirectory() {
+    if (!fs.existsSync(THUMBNAIL_DIRECTORY)) {
+        fs.mkdirSync(THUMBNAIL_DIRECTORY, { recursive: true });
+    }
+}
