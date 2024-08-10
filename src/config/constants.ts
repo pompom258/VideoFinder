a42bb40a7b17a548545e89618f0d8dab4a898c15
pompom <kitokito532@gmail.com>
2024-08-10 23:07:33 +0900

    拡張子追加

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 
 export const VIDEO_DIRECTORY = path.join(__dirname, "../../sample");
-export const VIDEO_EXTENSIONS = [".mp4", ".mkv", ".mov", ".avi", ".wmv"];
+export const VIDEO_EXTENSIONS = [".mp4", ".mkv", ".mov", ".avi", ".wmv", ".flv"];
 
 export const THUMBNAIL_DIRECTORY = path.join(__dirname, "../../public/thumbnails");
 export const THUMBNAIL_SIZE = "768x432";
