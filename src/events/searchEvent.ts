a497a49f53bfa3a7074cf494c918b9f0b295e099
pompom <kitokito532@gmail.com>
2024-09-29 22:01:40 +0900

    動画検索イベントハンドラの中で検索キーワードを受け取り

--- src/events/searchEvent.ts
+++ src/events/searchEvent.ts
@@ -2,14 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchInput = document.getElementById("search-input") as HTMLInputElement;
     const searchButton = document.getElementById("search-button") as HTMLButtonElement;
 
-    searchButton?.addEventListener('click', performSearch);
-    searchButton?.addEventListener('keypress', (e: KeyboardEvent) => {
+    searchButton?.addEventListener('click', () => {
+        performSearch(searchInput.value);
+    });
+    searchInput?.addEventListener('keypress', (e: KeyboardEvent) => {
         if (e.key === 'Enter') {
-            performSearch();
+            performSearch(searchInput.value);
         }
     })
 });
 
-function performSearch(): void {
-    alert("Not implemented yet!");
+function performSearch(keyword: string): void {
+    alert(`Not implemented yet!\nkeyword: ${keyword}`);
 }
