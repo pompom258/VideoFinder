94972538479a9f89c5ff52edbfe5063a9e4ed1f1
pompom <kitokito532@gmail.com>
2024-09-30 01:02:07 +0900

    動画情報を格納するDB操作クラス簡易実装

---
+++ src/videoStorage.ts
@@ -0,0 +1,35 @@
+import sqlite3 from "sqlite3";
+import { TABLE_NAME_VIDEOS, TABLE_PATH_VIDEOS } from "./config/constants";
+
+interface Video {
+    id: number;
+    name: string;
+    durationSeconds: number
+}
+
+export class VideoStorage {
+    private db = new sqlite3.Database(TABLE_PATH_VIDEOS);
+
+    public initialize() {
+        this.db.serialize(() => {
+            this.db.run(`DROP TABLE IF EXISTS ${TABLE_NAME_VIDEOS}`);
+            this.db.run(`CREATE TABLE IF NOT EXISTS ${TABLE_NAME_VIDEOS}(id INTEGER PRIMARY KEY, name TEXT, durationSeconds INTEGER)`);
+        });
+    }
+
+    public close() {
+        this.db.close();
+    }
+
+    public insert(id: number, name: string, durationSeconds: number) {
+        this.db.serialize(() => {
+            this.db.run(`INSERT INTO ${TABLE_NAME_VIDEOS}(id, name, durationSeconds) VALUES (?, ?, ?)`, id, name, durationSeconds);
+        });
+    }
+
+    public printAllRecords() {
+        this.db.each(`SELECT * FROM ${TABLE_NAME_VIDEOS}`, (err, row: Video) => {
+            console.log(`${row.id}, ${row.name}, ${row.durationSeconds}`);
+        });
+    }
+}
